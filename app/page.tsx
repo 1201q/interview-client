09e2676a144e80b034335a9d9c6da7ead16cabb5
hwang <1201q@naver.com>
2025-03-18 20:15:07 +0900

    test: 클라이언트 컴포넌트로 전달

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Test from "@/components/Test";
+
 export default async function Home() {
   const data = await fetch(`https://api.aiterview.tech/`);
-  const res = data;
-  console.log(res);
+  const res = data.text();
 
-  return <div>{process.env.NEXT_PUBLIC_API_URL}</div>;
+  return (
+    <div>
+      <Test test={res} />
+    </div>
+  );
 }
---
+++ components/Test.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const Test = ({ test }: { test: any }) => {
+  console.log(test);
+  return <div>1</div>;
+};
+
+export default Test;
