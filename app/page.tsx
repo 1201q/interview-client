0c42ea31206e2892cf0172c7576f9751327ec2a9
hwang <1201q@naver.com>
2025-03-18 19:51:51 +0900

    feat: env 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
-export default function Home() {
-  return <div>11232</div>;
+export default async function Home() {
+  // const data = await fetch(`${process.env.NEXT_PUBLIC_API_URL}`);
+
+  // console.log(await data.json());
+
+  return <div>{process.env.NEXT_PUBLIC_API_URL}</div>;
 }
