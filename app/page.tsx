f47edea9d2c964688f9890008b2ab34ab6d3c2a0
hwang <1201q@naver.com>
2025-03-18 20:02:12 +0900

    test: fetch 테스트

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default async function Home() {
-  // const data = await fetch(`${process.env.NEXT_PUBLIC_API_URL}`);
-
-  // console.log(await data.json());
+  const data = await fetch(`https://api.aiterview.tech/`);
+  const res = data;
+  console.log(res);
 
   return <div>{process.env.NEXT_PUBLIC_API_URL}</div>;
 }
