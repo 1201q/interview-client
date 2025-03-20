442b63e3cf6e7d7339ca2af93cf8024892641a56
hwang <1201q@naver.com>
2025-03-20 09:51:04 +0900

    test: webcam 비율 테스트

--- components/camera/WebcamClient.tsx
+++ components/camera/WebcamClient.tsx
@@ -1,46 +1,32 @@
 'use client';
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useRef } from 'react';
+import styles from './styles/webcam.module.css';
 import Webcam from 'react-webcam';
 
 const WebcamClient = () => {
   const webcamRef = useRef<Webcam>(null);
-  const [deviceId, setDeviceId] = useState([]);
-  const [devices, setDevices] = useState<MediaDeviceInfo[]>([]);
-
-  const handleDevices = useCallback(
-    (mediaDevices: MediaDeviceInfo[]) => {
-      return setDevices(
-        mediaDevices.filter((value) => value.kind === 'videoinput'),
-      );
-    },
-    [setDevices],
-  );
-
-  useEffect(() => {
-    navigator.mediaDevices.enumerateDevices().then(handleDevices);
-  }, [handleDevices]);
-
-  console.log(devices);
-  console.log(deviceId);
 
   return (
-    <>
-      {devices.map((device, key) => (
-        <div key={key} style={{ width: '800px' }}>
-          <Webcam
-            ref={webcamRef}
-            audio={false}
-            mirrored={true}
-            videoConstraints={{
-              aspectRatio: 1.71,
-              facingMode: 'user',
-              deviceId: device.deviceId,
-            }}
-          />
-          {device.label || `Device ${key + 1}`}
-        </div>
-      ))}
-    </>
+    <div className={styles.container}>
+      <Webcam
+        ref={webcamRef}
+        audio={false}
+        mirrored={true}
+        videoConstraints={{
+          aspectRatio: 1.71,
+          facingMode: 'user',
+        }}
+      />
+      <Webcam
+        ref={webcamRef}
+        audio={false}
+        mirrored={true}
+        videoConstraints={{
+          aspectRatio: 0.67777,
+          facingMode: 'user',
+        }}
+      />
+    </div>
   );
 };
 
