5556b3cf8d9f79797a0cddb504e6738668fcfb75
hwang <1201q@naver.com>
2025-04-06 17:37:36 +0900

    feat: others 페이지, 비로그인일 경우 로그인 안내 출력
    issue: #3, #7

--- app/step/select/(others)/need-login.tsx
+++ app/step/select/(others)/need-login.tsx
@@ -6,7 +6,7 @@ const NeedLogin = () => {
     <div className={styles.container}>
       <div className={styles.box}>
         <p className={styles.text}>해당 기능은</p>
-        <p className={styles.text}>로그인하고 사용할 수 있어요</p>
+        <p className={styles.text}>로그인 후 사용할 수 있어요</p>
         <div className={styles.buttonContainer}>
           <Link href={'/auth/login'}>
             <button className={styles.loginButton}>로그인하기</button>
