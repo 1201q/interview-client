f57941ccb3dcce70b80df66b3a90cfc2beb727f4
hwang <1201q@naver.com>
2025-04-20 13:51:26 +0900

    feat: 아래 컨트롤바와 사이드 추가한 질문 삭제 기능 추가
    issue: #7

--- app/test/_components/BottomController.tsx
+++ app/test/_components/BottomController.tsx
@@ -1,14 +1,28 @@
+'use client';
+
+import { useAtom } from 'jotai';
 import Button from './Button';
 import styles from './styles/bottom.controller.module.css';
+import { selectedQuestionsAtom } from '@/store/select';
 
 const BottomController = () => {
+  const [selectedData, setSelectedData] = useAtom(selectedQuestionsAtom);
+
   return (
     <div className={styles.container}>
       <div className={styles.leftContainer}>
-        <p>질문을 선택하세요</p>
+        {selectedData.length === 0 ? (
+          <p>질문을 선택하세요</p>
+        ) : (
+          <p>{selectedData.length}개 선택됨</p>
+        )}
       </div>
       <div className={styles.buttonContainer}>
-        <Button text="선택 초기화" disabled={false} />
+        <Button
+          text="선택 초기화"
+          disabled={false}
+          onClick={() => setSelectedData([])}
+        />
         <Button text="다음 단계로" disabled={true} color="blue" />
       </div>
     </div>
