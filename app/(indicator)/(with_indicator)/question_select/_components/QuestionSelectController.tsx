1730f17a64f456c1dea627b46e86615409df3efc
hwang <1201q@naver.com>
2025-05-24 10:34:56 +0900

    test: 임시로 질문 1개 체크시 넘어갈 수 있게 수정

--- app/(indicator)/(with_indicator)/question_select/_components/QuestionSelectController.tsx
+++ app/(indicator)/(with_indicator)/question_select/_components/QuestionSelectController.tsx
@@ -6,18 +6,22 @@ import { selectedQuestionsAtom } from '@/store/select';
 import { useRouter } from 'next/navigation';
 import BottomController from '@/components/common/BottomController';
 
+const NEXT_COUNT = 1;
+const MAX_COUNT = 15;
+
 const QuestionSelectController = () => {
   const router = useRouter();
   const [selectedData, setSelectedData] = useAtom(selectedQuestionsAtom);
 
-  const nextStep = selectedData.length < 5;
-  const remainingCount = 5 - selectedData.length;
+  const nextStep = selectedData.length < NEXT_COUNT;
+  const remainingCount = NEXT_COUNT - selectedData.length;
 
   const message = (() => {
     if (selectedData.length === 0) return '질문을 선택하세요';
-    if (selectedData.length < 5) return `${selectedData.length}개 선택됨`;
-    if (selectedData.length === 5) return '최대 15개까지 가능';
-    if (selectedData.length === 15) return '질문 최대 개수에 도달';
+    if (selectedData.length < NEXT_COUNT)
+      return `${selectedData.length}개 선택됨`;
+    if (selectedData.length === NEXT_COUNT) return '최대 15개까지 가능';
+    if (selectedData.length === MAX_COUNT) return '질문 최대 개수에 도달';
     return `${selectedData.length}개 선택됨`;
   })();
 
